f7447eb023ac65ffaaf11ecfca85fde5567c3c92
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-11-05 21:19:58 +0900

    基本のhtml構造を作成

--- src/index.php
+++ src/index.php
@@ -1,14 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>89ちゃんねる</title>
+  <!-- ここにCSSファイルやJavaScriptファイルのリンクを挿入 -->
+  <!-- <link rel="stylesheet" href="path/to/your/stylesheet.css">
+  <script src="path/to/your/script.js" defer></script> -->
 </head>
 
 <body>
-  テストです
+  <header>
+    <h1>89ちゃんねる</h1>
+    <p>お気に入りのトピックについて語り合いましょう。</p>
+  </header>
+
+  <nav>
+    <!-- ナビゲーションバーのリンク -->
+    <ul>
+      <li><a href="#home">ホーム</a></li>
+      <li><a href="#news">ニュース</a></li>
+      <li><a href="#contact">お問い合わせ</a></li>
+      <li><a href="#about">このサイトについて</a></li>
+    </ul>
+  </nav>
+
+  <main>
+    <!-- メインコンテンツ -->
+    <section id="home">
+      <h2>ホーム</h2>
+      <p>ようこそ89ちゃんねるへ。ここでは様々な話題についてディスカッションを行うことができます。</p>
+    </section>
+
+    <section id="news">
+      <h2>ニュース</h2>
+      <p>最新のニュースや更新情報をここでチェックしましょう。</p>
+    </section>
+
+    <!-- 他のセクションや記事を追加する場合はここに挿入 -->
+
+  </main>
+
+  <footer>
+    <p>&copy; 2023 89ちゃんねる</p>
+  </footer>
+
 </body>
 
 </html>

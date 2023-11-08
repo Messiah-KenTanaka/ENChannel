b7f985013830ac632fc237b91035c6eac7e56b55
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-11-09 08:15:22 +0900

    スレッド一覧のリファクタリング

--- src/index.php
+++ src/index.php
@@ -7,17 +7,8 @@ $error_msg = array();
 
 // INSERT処理 新規スレッドを立ち上げ
 include(__DIR__ . '/api/thread_add.php');
-
 // SELECT スレッド一覧を取得
-try {
-	$sql = "SELECT * FROM threads ORDER BY id DESC";
-	$stmt = $dbh->prepare($sql);
-	$stmt->execute();
-	$threads = $stmt->fetchAll();
-} catch (PDOException $e) {
-	// echo 'Error: ' . $e->getMessage();
-	$error_msg = 'スレッド一覧を読み込めませんでした。';
-}
+include(__DIR__ . '/api/thread_get.php');
 
 ?>
 
@@ -57,28 +48,7 @@ try {
 		</div>
 
 		<!-- スレッド一覧 -->
-		<section id="home" class="my-4">
-			<h2>スレッド一覧</h2>
-			<p>ようこそ89ちゃんねるへ。ここでは様々な話題についてディスカッションを行うことができます。</p>
-			<div class="row">
-				<?php foreach ($threads as $thread) : ?>
-					<div class="col-md-6">
-						<div class="card mb-4 shadow-sm">
-							<a href="views/pages/thread.php?id=<?= urlencode($thread['id']) ?>" class="card-body">
-								<h5 class="card-title"><?= h($thread['title']) ?></h5>
-								<div class="d-flex justify-content-between align-items-center">
-									<div class="btn-group">
-										<!-- <button type="button" class="btn btn-sm btn-outline-secondary">表示</button> -->
-										<!-- <button type="button" class="btn btn-sm btn-outline-secondary">編集</button> -->
-									</div>
-									<small class="text-muted"><?= date('Y年m月d日 H:i', strtotime($thread['created_at'])) ?></small>
-								</div>
-							</a>
-						</div>
-					</div>
-				<?php endforeach; ?>
-			</div>
-		</section>
+		<?php include(__DIR__ . '/views/parts/threads.php'); ?>
 
 	</main>
 
---
+++ src/views/parts/threads.php
@@ -0,0 +1,23 @@
+<!-- スレッド一覧 -->
+<section id="home" class="my-4">
+  <h2>スレッド一覧</h2>
+  <p>ようこそ89ちゃんねるへ。ここでは様々な話題についてディスカッションを行うことができます。</p>
+  <div class="row">
+    <?php foreach ($threads as $thread) : ?>
+      <div class="col-md-6">
+        <div class="card mb-4 shadow-sm">
+          <a href="views/pages/thread.php?id=<?= urlencode($thread['id']) ?>" class="card-body">
+            <h5 class="card-title"><?= h($thread['title']) ?></h5>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">表示</button> -->
+                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">編集</button> -->
+              </div>
+              <small class="text-muted"><?= date('Y年m月d日 H:i', strtotime($thread['created_at'])) ?></small>
+            </div>
+          </a>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</section>

06d5a7a77f5753813f70e95127608d1a87d85519
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-11-08 19:58:13 +0900

    ホーム画面へ遷移できるようにhrefを設定

--- src/views/parts/header.php
+++ src/views/parts/header.php
@@ -1,4 +1,6 @@
 <header class="bg-success text-white p-3 d-flex flex-column align-items-center">
-  <h1>89ちゃんねる</h1>
+  <a href="/">
+    <h1>89ちゃんねる</h1>
+  </a>
   <p>-スレッドを立ち上げて語り合おう-</p>
 </header>
--- src/views/parts/navbar.php
+++ src/views/parts/navbar.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">89ちゃんねる</a>
+    <a class="navbar-brand" href="/">89ちゃんねる</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#home">ホーム</a>
+          <a class="nav-link active" aria-current="page" href="/">ホーム</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#news">ニュース</a>

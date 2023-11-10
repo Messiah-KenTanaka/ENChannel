a7247431d258eca37665ddcab6d128809ba30832
KenTanaka <k.tanaka@apptime.co.jp>
2023-11-10 17:04:28 +0900

    head, scriptをコンポーネント化

--- src/views/pages/thread.php
+++ src/views/pages/thread.php
@@ -12,13 +12,7 @@ include(__DIR__ . '../../../api/thread_get.php');
 <!DOCTYPE html>
 <html lang="ja">
 
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>89ちゃんねる</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-	<link rel="stylesheet" href="/views/css/style.css">
-</head>
+<?php include(__DIR__ . '../../parts/head.php'); ?>
 
 <body>
 	<!-- ヘッダーを読み込む -->
@@ -39,9 +33,6 @@ include(__DIR__ . '../../../api/thread_get.php');
 
 	<!-- フッターを読み込む -->
 	<?php include(__DIR__ . '../../parts/footer.php'); ?>
-
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-	<script src="/views/js/script.js"></script>
 </body>
 
 </html>
--- src/views/parts/footer.php
+++ src/views/parts/footer.php
@@ -1,3 +1,6 @@
 <footer class="bg-dark text-white text-center p-3 fixed-bottom">
   <span>&copy; <?php echo date("Y"); ?> 89ちゃんねる</span>
 </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<script src="/views/js/script.js"></script>

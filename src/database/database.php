b4e76ad365985cac9273d50712a62dc6466db6df
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-11-11 20:55:36 +0900

    herokuにデプロイできるように設定
    DB接続、compoerなど

--- src/database/database.php
+++ src/database/database.php
@@ -1,12 +1,27 @@
 <?php
 
-$dsn = 'mysql:dbname=enchannel;host=db;port=3306';
-$user = 'user';
-$password = 'password';
+$dbUrl = getenv("JAWSDB_URL");
 
+if ($dbUrl) {
+  // Herokuの本番環境
+  $urlParts = parse_url($dbUrl);
+
+  $dbHost = $urlParts['host'];
+  $dbPort = $urlParts['port'];
+  $dbName = ltrim($urlParts['path'], '/');
+  $dbUser = $urlParts['user'];
+  $dbPass = $urlParts['pass'];
+
+  $dsn = "mysql:host=$dbHost;port=$dbPort;dbname=$dbName";
+} else {
+  // ローカル環境
+  $dsn = "mysql:dbname=enchannel;host=db;port=3306";
+  $dbUser = 'user';
+  $dbPass = 'password';
+}
 // DBへ接続
 try {
-  $dbh = new PDO($dsn, $user, $password);
+  $dbh = new PDO($dsn, $dbUser, $dbPass);
   // print("データベースの接続に成功しました");
 } catch (PDOException $e) {
   print("データベースの接続に失敗しました" . $e->getMessage());

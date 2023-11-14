ed48ea422c15d95058bcd59a88dc773de8d6a0ec
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-11-14 21:59:45 +0900

    bootstrapのアイコンを追加

--- src/views/parts/thread_form.php
+++ src/views/parts/thread_form.php
@@ -1,6 +1,9 @@
 <!-- スレッドを立ち上げる -->
 <div class="container my-3 p-3 bg-success-subtle">
-  <h2>スレッドを立ち上げる</h2>
+  <h2>
+    <i class="bi bi-threads-fill"></i>
+    <span class="mr-2">スレッドを立ち上げる</span>
+  </h2>
   <form action="<?= $_SERVER['SCRIPT_NAME'] ?>" method="POST">
     <div class="mb-3">
       <input type="text" class="form-control" id="title" name="title" placeholder="スレッド名を入力..." minlength="0" maxlength="128" required>
--- src/views/parts/threads.php
+++ src/views/parts/threads.php
@@ -1,19 +1,19 @@
-<!-- スレッド一覧 -->
 <section id="home" class="my-4">
-  <!-- <div class="text-white"> -->
   <h2>スレッド一覧</h2>
   <p>ようこそ89ちゃんねるへ。ここでは様々な話題についてディスカッションを行うことができます。</p>
-  <!-- </div> -->
   <div class="row">
     <?php foreach ($threads as $thread) : ?>
-      <div class="col-md-6">
-        <div class="card mb-4 shadow-sm">
-          <a href="views/pages/thread.php?id=<?= urlencode($thread['id']) ?>" class="card-body">
-            <h5 class="card-title"><?= h($thread['title']) ?></h5>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">表示</button> -->
-                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">編集</button> -->
+      <div class="col-md-6 col-lg-4 mb-4">
+        <div class="card h-100 shadow-sm rounded hover-shadow">
+          <a href="views/pages/thread.php?id=<?= urlencode($thread['id']) ?>" class="card-body text-decoration-none">
+            <h5 class="card-title text-dark">
+              <i class="bi bi-threads ml-1"></i>
+              <?= h($thread['title']) ?>
+            </h5>
+            <div class="d-flex justify-content-between align-items-center mt-3">
+              <div>
+                <i class="bi bi-chat-dots"></i>
+                <small><?= $thread['comment_count'] ?>件</small>
               </div>
               <small class="text-muted"><?= date('Y年m月d日 H:i', strtotime($thread['created_at'])) ?></small>
             </div>

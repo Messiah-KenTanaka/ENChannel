c67463ec5c8c83105574cb2ed10d827fbb479bca
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-11-16 08:17:57 +0900

    バッチ処理、途中を一旦コミット

--- src/app/batch/script.php
+++ src/app/batch/script.php
@@ -1,10 +1,24 @@
 <?php
 
-include(__DIR__ . '../../../database/database.php');
-include(__DIR__ . '../../functions/util.php');
+include(__DIR__ . '/../../functions/util.php');
+include(__DIR__ . '/../../database/database.php');
 
 // ログメッセージの作成
-$logMessage = "Batch script execute at: " . "\n";
+$logMessage = "バッチ処理開始: " . "\n";
+writeLog($logMessage);
+
+// スレッドみん紐づくコメント数をカウント
+$sql = "SELECT t.id, t.title, COUNT(c.thread_id) AS COUNT
+        FROM threads t
+          JOIN comments c
+          ON t.id = c.thread_id
+        GROUP BY c.thread_id";
+$stmt = $dbh->prepare($sql);
+$datas = $stmt->execute();
+
+writeLog($datas);
+
 
 // ログファイルへの書き込み
+$logMessage = "バッチ処理終了: " . "\n";
 writeLog($logMessage);
--- src/app/functions/util.php
+++ src/app/functions/util.php
@@ -10,9 +10,15 @@ if (!function_exists('h')) {
 }
 
 // ログをファイルに出力
-function writeLog($message) {
+function writeLog($message)
+{
   $data = new DateTime();
   $timestamp = $data->format('Y-m-d H:i:s');
+  // 引数が配列またはオブジェクトの場合、print_rを使用して文字列に変換
+  if (is_array($message) || is_object($message)) {
+    $message = print_r($message, true);
+  }
+
   $logMessage = $timestamp . '：' . $message . "\n";
 
   file_put_contents(__DIR__ . '/../../logs/app.log', $logMessage, FILE_APPEND);
